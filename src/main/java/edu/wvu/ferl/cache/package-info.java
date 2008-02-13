fc974ee8af194a096361f015a1bd3330e45f1b2a
jared.bunting <devnull@localhost>
2008-02-13 17:12:18 +0000

    * Added copyright and ownership information

--- src/main/java/edu/wvu/ferl/cache/package-info.java
+++ src/main/java/edu/wvu/ferl/cache/package-info.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2008 West Virginia University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 /**
  * This package is a simple abstraction for providing a lazy-loading type cache.  The idea is that there exists a cache
  * in which objects are stored in a standard key-value scheme.  A client does not put objects into this cache.  A
